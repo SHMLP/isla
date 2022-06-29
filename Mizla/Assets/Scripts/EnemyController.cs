4e5b29953c37d2a0f99f625273cb3f79e3797ba3
unknown <SENA@MEDAPRCCSFSD345.FormacionSena.RED>
2022-06-29 18:00:59 -0500

    SecorrigioUnpocoelcodigo

--- Mizla/Assets/Scripts/EnemyController.cs
+++ Mizla/Assets/Scripts/EnemyController.cs
@@ -24,36 +24,24 @@ public class EnemyController : MonoBehaviour
     private void Update()
     {
         
-        if (Vector3.Distance(transform.position, jugador.position) <= actackrange+1 && Vector3.Dot((jugador.position - transform.position).normalized, transform.TransformDirection(Vector3.forward).normalized) >= 0.5)
-        //if (Physics.Raycast(vista.position,vista.forward,out RaycastHit hit,actackrange))
+        //if (Vector3.Distance(transform.position, jugador.position) <= actackrange+1 && Vector3.Dot((jugador.position - transform.position).normalized, transform.TransformDirection(Vector3.forward).normalized) >= 0.5)
+        if (Physics.Raycast(vista.position,vista.forward,out RaycastHit hit,actackrange))
         {
             tiempoDeAtaque += Time.deltaTime;
-            print("muere1");
-
-       
-            //if (hit.transform.name == "Jugador" )
-            //{
-                //print("muere2");
-                enemyMove.Move(0, 0);
+            if (hit.transform.name == "Jugador" )
+            {
                 animacion.SetFloat("SpeedMovement", 0);
-                //print("tiempo"+tiempoDeAtaque);
-                //print(Vector3.Distance(transform.position, jugador.position));
                 if (tiempoDeAtaque >= 3f && Vector3.Distance(transform.position, jugador.position) <= actackrange+1)
                 {
-                    
-                    print("muere3");
-                   
+                    enemyMove.Move(0, 0);
                     animacion.SetFloat("Atacar", 1);
                     tiempoDeAtaque = 0;
-                  
-
                 }
-            //}
+            }
            
         }
         else if (Vector3.Distance(transform.position, jugador.position) <= range)
         {
-            
             if (Vector3.Dot((jugador.position - transform.position).normalized, transform.TransformDirection(Vector3.forward).normalized) >= 0.5)
             {
                 tiempoDeAtaque = 0;
@@ -63,11 +51,11 @@ public class EnemyController : MonoBehaviour
                     
                 if (Vector3.Dot((jugador.position - transform.position).normalized, transform.TransformDirection(Vector3.right).normalized) >= 0)
                 {
-                    enemyMove.RotarPersonaje(giroEnemigo*Mathf.Pow(((jugador.position - transform.position).normalized - transform.TransformDirection(Vector3.forward).normalized).magnitude, 10f));
+                    enemyMove.RotarPersonaje(giroEnemigo*Mathf.Pow(((jugador.position - transform.position).normalized - transform.TransformDirection(Vector3.forward).normalized).magnitude, 5f));
                 }
                 else
                 {
-                    enemyMove.RotarPersonaje(-giroEnemigo * Mathf.Pow(((jugador.position - transform.position).normalized - transform.TransformDirection(Vector3.forward).normalized).magnitude, 10f));
+                    enemyMove.RotarPersonaje(-giroEnemigo * Mathf.Pow(((jugador.position - transform.position).normalized - transform.TransformDirection(Vector3.forward).normalized).magnitude, 5f));
                 }
                 
             }
